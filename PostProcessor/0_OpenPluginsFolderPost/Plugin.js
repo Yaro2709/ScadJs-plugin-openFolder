4d30bb2a09d5f196e9e2aa1b0fd5acb43ac6dfb1
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2024-08-11 17:58:10 +0300

    v1.0.0

---
+++ PostProcessor/0_OpenPluginsFolderPost/Plugin.js
@@ -0,0 +1,24 @@
+var TITLE = "OpenPluginsFolderPost";
+
+var WshShell = new ActiveXObject("Wscript.Shell");
+
+function Plugin_Cancel(engine) {
+    if (engine) {
+        engine.Cancel();
+    }
+}
+
+function Plugin_Execute(engine) {
+    try {
+        var folderNamePreProcessor = "0_"+ TITLE + "\\";
+        var pluginPath = engine.GetPluginRootDirectory();
+        var folderPath = pluginPath.slice(0, pluginPath.length - folderNamePreProcessor.length - 1);
+        var pathOpen = "\"" +folderPath.replace(/\\/g, "/")+ "\""
+
+        WshShell.Run(pathOpen);
+
+    } catch (e) {
+        WshShell.Popup("Ошибка!", 0, TITLE);
+        engine.Cancel(e);
+    }
+}
